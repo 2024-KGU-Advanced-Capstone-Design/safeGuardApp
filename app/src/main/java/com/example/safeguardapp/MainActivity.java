8e31db40788406885471c4c24331072f188f6a36
김영중 <s000824@naver.com>
2024-04-01 16:04:52 +0900

    회원가입 화면 1차 적용

--- app/src/main/java/com/example/safeguardapp/MainActivity.java
+++ app/src/main/java/com/example/safeguardapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.safeguardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // fragmentSignUp을 추가
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, new fragmentSignUp());
+        transaction.addToBackStack(null); // 백 스택에 추가하여 뒤로가기 버튼 동작을 처리할 수 있도록 함
+        transaction.commit();
     }
 }
