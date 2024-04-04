0d591ea1238197b948129fd6d33b2a15ec1168ec
kim0jung <116993837+kim0jung@users.noreply.github.com>
2024-04-04 10:28:08 +0900

    [화면 연결] 완료 (#3)
    * 회원가입 화면 1차 적용
    
    * 회원가입 화면 구현(버튼 미구현)
    
    * 버튼 비활성 기능 수정
    
    * login_signup connect(not yet)
    
    * activity_main.xml 파일 충돌 해결
    
    * login -> signup 화면전환 완료

---
+++ app/src/main/java/com/example/safeguardapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.safeguardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.activity_main, new loginPageFragment());
+        transaction.commit();
+
+    }
+
+
+
+}
