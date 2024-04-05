a42240466e5ef893e89bebfe9e448e396e7728fd
kim0jung <116993837+kim0jung@users.noreply.github.com>
2024-04-05 09:13:19 +0900

    BottomNavigationView 초기 구현 (#16)
    Co-authored-by: cm2hcm2h <cm2hcm2h@naver.com>

--- app/src/main/java/com/example/safeguardapp/MainActivity.java
+++ app/src/main/java/com/example/safeguardapp/MainActivity.java
@@ -2,20 +2,29 @@ package com.example.safeguardapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationBarItemView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.util.FusedLocationSource;
 
+import javax.net.ssl.HostnameVerifier;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    MapFragment mapFragment;
+    GroupFragment groupFragment;
+    SettingFragment settingFragment;
 
     private FusedLocationSource locationSource;
     private NaverMap mNaverMap;
@@ -31,12 +40,39 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //  <----- BottomNavigationView 구현 중 ----->
+        mapFragment = new MapFragment();
+        groupFragment = new GroupFragment();
+        settingFragment = new SettingFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers, mapFragment).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_navigationview);
+        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.home){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, mapFragment).commit();
+                    return true;
+                }else if(item.getItemId() == R.id.setting){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, settingFragment).commit();
+                    return true;
+                }else if(item.getItemId() == R.id.group){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, groupFragment).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
+        // <----- BottomNavigationView 구현 중 ----->
+
         //지도 객체 생성
         FragmentManager fragmentManager = getSupportFragmentManager();
-        MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map);
+        /* 이 코드 때문에 한 화면에 지도가 두 번 생성 되는 듯?
+        MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.home); */
         if(mapFragment == null){
             mapFragment = MapFragment.newInstance();
-            fragmentManager.beginTransaction().add(R.id.map, mapFragment).commit();
+            fragmentManager.beginTransaction().add(R.id.home, mapFragment).commit();
         }
 
         //getMapAsync 호출해 비동기로 onMapReady 콜백 메서드 호출
