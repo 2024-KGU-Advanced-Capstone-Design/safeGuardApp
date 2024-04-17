3a874b9dd6b5914fa296297343f2d124b4794876
Jhong <ghddl23@naver.com>
2024-04-17 19:46:46 +0900

    login 구현ing...

--- app/src/main/java/com/example/safeguardapp/UserRetrofitInterface.java
+++ app/src/main/java/com/example/safeguardapp/UserRetrofitInterface.java
@@ -3,12 +3,12 @@ package com.example.safeguardapp;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface UserRetrofitInterface {
-
-    //@Get ("user")
-    //Call<UserDTO> getUser();
+    @POST("login")
+    Call<LoginResponse> login(@Body LoginRequest request);
 
     @POST("signup")
     Call<ResponseBody> signUp(@Body SignUpRequestDTO jsonUser);
--- app/src/main/java/com/example/safeguardapp/loginPageFragment.java
+++ app/src/main/java/com/example/safeguardapp/loginPageFragment.java
@@ -1,20 +1,39 @@
 package com.example.safeguardapp;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.gson.Gson;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class loginPageFragment extends Fragment {
     private startScreenActivity startScreenActivity;
+    private RetrofitClient retrofitClient;
+    private UserRetrofitInterface userRetrofitInterface;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
 
     @Nullable
     @Override
@@ -27,20 +46,18 @@ public class loginPageFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        EditText editTextID = view.findViewById(R.id.editTextID);
+        EditText editTextPW = view.findViewById(R.id.editTextPW);
+        CheckBox checkBox = view.findViewById(R.id.autoLogin);
+
         // 로그인 버튼에 대한 클릭 이벤트 처리
         view.findViewById(R.id.buttonLogin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText editTextID = view.findViewById(R.id.editTextID);
                 String id = editTextID.getText().toString();
-
-                EditText editTextPW = view.findViewById(R.id.editTextPW);
                 String pw = editTextPW.getText().toString();
-                // ------> 여기에 로그인 처리 코드 추가
 
-                // 로그인 성공 했다 치고 startScreenActivity -> MainActivity로 이동 구현
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                startActivity(intent);
+                LoginResponse(id,pw);
             }
         });
 
@@ -74,4 +91,63 @@ public class loginPageFragment extends Fragment {
             }
         });
     }
+
+    public void LoginResponse(String id, String pw){
+        String login = "login";
+        String loginType = "Member";
+
+        //loginRequest에 id,pw 저장
+        LoginRequest loginRequest = new LoginRequest(id,pw,loginType);
+
+        //retrofit 생성
+        retrofitClient = RetrofitClient.getInstance(login);
+        userRetrofitInterface = RetrofitClient.getInstance(login).getUserRetrofitInterface();
+
+        Log.e("POST","client 생성");
+
+        Gson gson = new Gson();
+        String userInfo = gson.toJson(loginRequest);
+
+        Log.e("JSON",userInfo);
+
+        Call<LoginResponse> call = userRetrofitInterface.login(loginRequest);
+        call.clone().enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                //통신 성공
+                if(response.isSuccessful()&&response.body()!=null){
+                    Log.e("POST","통신 성공");
+                    LoginResponse result = response.body();
+                    String resultCode = result.getResultCode();
+
+                    String success="200"; //성공
+                    String errorID="300"; //아이디X
+                    String errorPW="400"; //비밀번호 불일치
+
+                    if(resultCode.equals(success)){
+                        Log.e("POST","로그인 성공");
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                    else if (resultCode.equals(errorID)) {
+                        Log.e("POST","아이디 에러");
+                    }
+                    else if (resultCode.equals(errorPW)) {
+                        Log.e("POST","패스워드 에러");
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                Log.e("POST","에러");
+            }
+        });
+
+    }
+    //자동 로그인 유저
+    public void checkAutoLogin(){
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        startActivity(intent);
+    }
+
 }
