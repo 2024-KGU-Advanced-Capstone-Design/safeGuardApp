b3701cd53323650f4b8350e045cd31ffb8ab07a1
김영중 <s000824@naver.com>
2024-05-17 15:46:24 +0900

    [기능 구현] child 화면 구현 및 로그아웃 기능 구현

--- app/src/main/java/com/example/safeguardapp/Child/ChildMainActivity.java
+++ app/src/main/java/com/example/safeguardapp/Child/ChildMainActivity.java
@@ -1,11 +1,4 @@
-package com.example.safeguardapp;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+package com.example.safeguardapp.Child;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
@@ -17,9 +10,14 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import com.example.safeguardapp.Group.GroupFragment;
+import com.example.safeguardapp.Child.ChildMainActivity;
 import com.example.safeguardapp.Notice.NoticeFragment;
-import com.example.safeguardapp.Setting.SettingFragment;
+import com.example.safeguardapp.R;
 import com.google.android.material.navigation.NavigationBarView;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
@@ -28,11 +26,9 @@ import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.util.FusedLocationSource;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    public MapFragment mapFragment;
-    public GroupFragment groupFragment;
-    public NoticeFragment noticeFragment;
-    public SettingFragment settingFragment;
+public class ChildMainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    public MapFragment childMapFragment;
+    public ChildSettingFragment childSettingFragment;
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
     private FusedLocationSource locationSource;
@@ -47,43 +43,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_child_main);
         locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
         // -----v----- BottomNavigationView 구현 -----v-----
-        mapFragment = new MapFragment();
-        groupFragment = new GroupFragment();
-        noticeFragment = new NoticeFragment();
-        settingFragment = new SettingFragment();
+        childMapFragment = new MapFragment();
+        childSettingFragment = new ChildSettingFragment();
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.containers, mapFragment).commit();
 
-        NavigationBarView navigationBarView = findViewById(R.id.bottom_navigationview);
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers, childMapFragment).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.child_bottom_navigationview);
         navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (item.getItemId() == R.id.map){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, mapFragment).commit();
-                    mapFragment.getMapAsync(MainActivity.this);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, childMapFragment).commit();
+                    childMapFragment.getMapAsync(ChildMainActivity.this);
                     // mapFragment를 실행시 mapModeNav 보이게 설정
                     LinearLayout mapModeNav = findViewById(R.id.mapModeNav);
                     mapModeNav.setVisibility(View.VISIBLE);
                     return true;
-                }else if(item.getItemId() == R.id.setting){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, settingFragment).commit();
+                }else if(item.getItemId() == R.id.setting) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, childSettingFragment).commit();
                     // SettingFragment로 실행시 mapModeNav를 사라지게 설정
-                    LinearLayout mapModeNav = findViewById(R.id.mapModeNav);
-                    mapModeNav.setVisibility(View.GONE);
-                    return true;
-                }else if(item.getItemId() == R.id.notice){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, noticeFragment).commit();
-                    // NoticeFragment로 실행시 mapModeNav를 사라지게 설정
-                    LinearLayout mapModeNav = findViewById(R.id.mapModeNav);
-                    mapModeNav.setVisibility(View.GONE);
-                    return true;
-                }else if(item.getItemId() == R.id.group){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, groupFragment).commit();
-                    // GroupFragment로 넘어가면 mapModeNav를 사라지게 설정
                     LinearLayout mapModeNav = findViewById(R.id.mapModeNav);
                     mapModeNav.setVisibility(View.GONE);
                     return true;
@@ -91,20 +75,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return false;
             }
         });
-        // -----^----- BottomNavigationView 구현 -----^-----
 
-        //지도 객체 생성
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-        /* 이 코드 때문에 한 화면에 지도가 두 번 생성 되는 듯?
-        MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.home); */
-//        if(mapFragment == null){
-//            mapFragment = MapFragment.newInstance();
-//            fragmentManager.beginTransaction().add(R.id.map, mapFragment).commit();
-//        }
-
-        //getMapAsync 호출해 비동기로 onMapReady 콜백 메서드 호출
-        //onMapReady에서 NaverMap 객체를 받음.
-        mapFragment.getMapAsync(this);
+        childMapFragment.getMapAsync(this);
 
         //위치를 반환하는 구현체인 FusedLocationSource 생성
         locationSource = new FusedLocationSource(this, PERMISSION_REQUEST_CODE);
@@ -112,7 +84,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
-
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.map_types,
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -143,7 +114,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // 권한 확인, 결과는 onRequestPermissionResult 콜백 메서드 호출
         ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE);
-
     }
 
     @Override
